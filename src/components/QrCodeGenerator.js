ae62fd85d73bca864ea277705cb34c7f4183dfc1
Prajwalabraham <prajwalabraham.21@gmail.com>
2022-12-28 15:37:13 +0530

    updATE

--- src/components/QrCodeGenerator.js
+++ src/components/QrCodeGenerator.js
@@ -5,6 +5,11 @@ import { useState } from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
 import './QrCodeGenerator.css'
 import Select from 'react-select';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
 import makeAnimated from 'react-select/animated';
 
 function QrCodeGenerator() {
@@ -15,6 +20,9 @@ function QrCodeGenerator() {
     });
     const [team, setTeam] = useState('');
   const animatedComponents = makeAnimated();
+  const [err, setErr] = useState(false);
+  const [errMsg, seterrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
 
   const teams = [
     {value:'Greeters', label:'Greeters'},
@@ -80,13 +88,14 @@ function QrCodeGenerator() {
       .then(res => {
         console.log(res);
         if(res.status==201){
-          alert('Volunteer Successfully Created')
+          setSuccess(true)
         }
       })
       .catch(err => {
         console.log(err);
         if (err.response.status==406) {
-          alert("The volunteer already exists!");
+          setErr(true)
+          seterrMsg("The volunteer already exists!")
         }
         else{
           alert('There is an internal Server error. Kindly report to the IT team')
@@ -102,7 +111,7 @@ function QrCodeGenerator() {
     }
   }
 
-    const email = document.getElementById("mail");
+const email = document.getElementById("mail");
 if(email){
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
@@ -113,14 +122,73 @@ email.addEventListener("input", (event) => {
   }
 });
 }
-    
+const name = document.getElementById("name");
+if(name){
+name.addEventListener("input", (event) => {
+  if (name.validity.patternMismatch) {
+    name.setCustomValidity("Name should be in format: Jesus Christ");
+    name.reportValidity();
+  } else {
+    name.setCustomValidity("");
+  }
+});
+}
+
+const phone = document.getElementById("phone");
+if(phone){
+phone.addEventListener("input", (event) => {
+  if (phone.validity.patternMismatch) {
+    phone.setCustomValidity("Phone should be of 10 digits");
+    phone.reportValidity();
+  } else {
+    phone.setCustomValidity("");
+  }
+});
+}
+
+const handleError = (e) =>{
+  setErr(false)
+  seterrMsg('')
+}
+const handleSuccess = (e) =>{
+  setSuccess(false)
+}
       // download QR code
     
   return (
     <div className='login-box'>
+      {err ? 
+       <div>
+       <span><img src="https://user-images.githubusercontent.com/74299799/209782507-c470b66c-4666-481a-a187-01ddc9992625.png" alt={<ErrorOutlineOutlinedIcon sx={{ fontSize: 210 }}  style={{ color: "red" }} />} /></span>
+       <br/>
+       <Stack sx={{ width: '100%' }} spacing={2}>
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {errMsg}
+        </Alert>
+      </Stack>
+       <button type="" onClick = {handleError}>Back</button>
+
+      </div>
+      :
+      <>
+{success? 
+  <div>
+           <span><img src="https://user-images.githubusercontent.com/74299799/209782500-1ef43bd6-dadf-478c-b1e2-b89ecf05428d.png" alt={<CheckCircleOutlinedIcon sx={{ fontSize: 210 }}  style={{ color: "green" }} />} /></span>
+           <br/>
+           <Stack sx={{ width: '100%' }} spacing={2}>
+            <Alert severity="success">
+              <AlertTitle>Success</AlertTitle>
+              <strong>Successfully Submitted</strong>
+            </Alert>
+          </Stack>
+           <button type="" onClick = {handleSuccess}>Back</button>
+
+          </div>
+: 
         <form onSubmit={GenerateQRCode}>
-          <input name='name'  placeholder='Full Name' value={state.name} onChange={handleChange} required />
-            <input name='phone'  placeholder='Phone' type='tel' pattern="(6|7|8|9)\d{9}$" value={state.phone} onChange={handleChange} required='true'/>
+          <input name='name' id='name' placeholder='Full Name' pattern="^[a-z]([-']?[a-z]+)*( [a-z]([-']?[a-z]+)*)+$" value={state.name} onChange={handleChange} required />
+            <input name='phone' id='phone' placeholder='Phone' type='tel' pattern="(6|7|8|9)\d{9}$" value={state.phone} onChange={handleChange} required='true'/>
             <input name='email' id='mail' placeholder='Email' type='email'  value={state.email} onChange={handleChange} required />
               <br/>
             <Select
@@ -133,7 +201,10 @@ email.addEventListener("input", (event) => {
               onChange={handleSelect}
             />
       			<button type='submit' >Generate</button>
-        </form>       
+        </form>  
+}     
+        </>
+}
         {qr && <>
 				<img src={qr} alt='' />
         <a href={qr} download={`${state.name}.png`} ><button value="Download" >Download</button></a>
@@ -142,9 +213,8 @@ email.addEventListener("input", (event) => {
       {loading ?  <CircularProgress /> :
       <button onClick={handleSubmit}>Submit</button>}
       <br/>       
-        
     </div>
-        
+
   )
 }
 
